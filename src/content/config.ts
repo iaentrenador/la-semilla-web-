f250fae935d28973e6ee47cee071891453b8e466
iaentrenador <gunmachine786@gmail.com>
2026-05-25 14:39:28 -0300

    Add tomosCollection with schema definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,18 @@
+import { z, defineCollection } from 'astro:content';
+
+const tomosCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    volumen: z.number(),
+    fecha: z.string(),
+    descripcion: z.string(),
+    portada: z.string().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  'tomos': tomosCollection,
+};
 
